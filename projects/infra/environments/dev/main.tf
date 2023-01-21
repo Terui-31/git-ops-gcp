36919435ee8f8a95f16f9e9220115ce93582190d
Takahiro Terui <takahiroterui@TakahironoMacBook-Air.local>
2023-01-22 01:10:13 +0900

    test16

--- projects/infra/environments/dev/main.tf
+++ projects/infra/environments/dev/main.tf
@@ -1,49 +1,52 @@
 locals {
-    extra_apis = [
+    services = toset([
         "bigquery.googleapis.com",
-        "dlp.googleapis.com"
-    ]
+        "dlp.googleapis.com",
+        "cloudbuild.googleapis.com",
+        "iam.googleapis.com",
+    ])
     subnet_front = "projects/${var.sharedvpc_project}/regions/${var.sharedvpc_region}/subnetworks/${var.sharedvpc_front}"
 }
 
+# enable gcp apis
 resource "google_project_service" "apis" {
     project = var.project_id
-    disable_dependent_services = true
-    for_each = toset(local.extra_apis)
+    for_each = local.services
     service = each.value
+    disable_dependent_services = true
 }
 
-resource "google_compute_shared_vpc_service_project" "vpc" {
-    host_project = var.sharedvpc_project
-    service_project = var.project_id
-}
+# resource "google_compute_shared_vpc_service_project" "vpc" {
+#     host_project = var.sharedvpc_project
+#     service_project = var.project_id
+# }
 
-module "web_server" {
-    source = "../../modules/web_server"
+# module "web_server" {
+#     source = "../../modules/web_server"
 
-    project = var.project_id
-    env = var.env
-    zone = var.zone
-    subnet = local.subnet_front
+#     project = var.project_id
+#     env = var.env
+#     zone = var.zone
+#     subnet = local.subnet_front
 
-    depends_on = [
-      google_compute_shared_vpc_service_project.vpc
-    ]
-}
+#     depends_on = [
+#       google_compute_shared_vpc_service_project.vpc
+#     ]
+# }
 
-module "lb-http" {
-    source = "../../modules/lb-http"
+# module "lb-http" {
+#     source = "../../modules/lb-http"
 
-    project_id = var.project_id
-    env = var.env
-    vpc_host_project = var.sharedvpc_project
-    vpc_network = var.sharedvpc_name
+#     project_id = var.project_id
+#     env = var.env
+#     vpc_host_project = var.sharedvpc_project
+#     vpc_network = var.sharedvpc_name
 
-    service_port = 80
-    service_port_name = "port-http-${var.env}"
-    backend_mig = module.web_server.mig.id
+#     service_port = 80
+#     service_port_name = "port-http-${var.env}"
+#     backend_mig = module.web_server.mig.id
 
-    depends_on = [
-      module.web_server
-    ]
-}
+#     depends_on = [
+#       module.web_server
+#     ]
+# }
