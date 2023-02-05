2a2d494b4c39a444cd770ec7f04cb6a88a6b9c73
Takahiro Terui <takahiroterui@TakahironoMacBook-Air.local>
2023-02-05 14:59:06 +0900

    test31-2

--- projects/infra/modules/variables.tf
+++ projects/infra/modules/variables.tf
@@ -1,13 +1,13 @@
-# variable "env" {
-#     type  = string
-# }
+variable "env" {
+    type  = string
+}
 
-# variable "location" {
-#     type  = string
-# }
+variable "location" {
+    type  = string
+}
 
-# output "common_vars" {
-#     type = object({
-#         project_id = map(string)
-#     })
-# }
+output "common_vars" {
+    type = object({
+        project_id = map(string)
+    })
+}
